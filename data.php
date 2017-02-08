0f03f8a79c1e27f30363591a850d677a69f60433
rahul2240 <rahulsingh2240@gmail.com>
2017-02-08 20:29:01 +0530

    project 1 for ncs

---
+++ data.php
@@ -0,0 +1,20 @@
+<?php
+
+include 'config.php';
+
+$q = $_REQUEST["q"];
+$q = strtolower($q);
+
+$query= mysql_query("SELECT * FROM names WHERE name LIKE '$q%'");
+
+while($row=mysql_fetch_array($query))
+{
+	$a[]=$row['name'];
+	
+}
+ foreach($a as $r) {
+  $hint .= ", $r";
+ }
+ 
+ echo $hint;
+?>
