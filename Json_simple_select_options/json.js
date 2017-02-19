927972f29c95525912bca1883221acbd513c9023
Rahul <rahulsingh2240@gmail.com>
2017-02-19 23:04:41 +0530

    Add files via upload

--- Json_simple_select_options/json.js
+++ Json_simple_select_options/json.js
@@ -1,1 +1,12 @@
+var content = {
+	"select": ["nil"] ,
+		"num": ["1","2","3"],
+			"A": ["A","B","C","D"]
+};
 
+function selectedvalue(val)
+{ 
+var demos = document.getElementById("demo");
+
+demos.innerHTML = content[val];
+}
