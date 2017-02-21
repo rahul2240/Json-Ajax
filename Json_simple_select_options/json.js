43fc08dbb2b4947d2c9fcd504b581e6a0a72fcec
Rahul <rahulsingh2240@gmail.com>
2017-02-21 22:59:00 +0530

    Add files via upload

--- Json_simple_select_options/json.js
+++ Json_simple_select_options/json.js
@@ -1,8 +1,23 @@
 var subject = {
 	"select": ["nil"],
 		"IT": ["MATH","PHY","CHEM"],
-		"CS": ["BIO","C","MATHS-2","PHY-2"],
+		"CS": ["BIO","C","PHY-2"],
 		"ME": ["THERMO","MECH"]
+};
+
+var semester = {
+	
+	"nil":"nil",
+	"MATH":["1","2","3"],
+	"PHY":"2",
+	"CHEM":"3",
+	"BIO":"2",
+	"C":"1",
+	"PHY-2":"3",
+	"THERMO":"4",
+	"MECH":"3"
+		
+	
 };
 
 function selectsubject(val)
@@ -12,4 +27,15 @@ function selectsubject(val)
 		optionret += "<option value=" + subject[val][x]+">" + subject[val][x] + "</option>";
 	}
     document.getElementById("subjects").innerHTML = optionret;
+}
+
+function selectsemester(valu)
+{
+	optionretu="";
+	for(var y=0; y<semester[valu].length; y++){
+		optionretu += "<option value=" + semester[valu][y]+">" + semester[valu][y] + "</option>";
+	}
+    document.getElementById("semester").innerHTML = optionretu;
+	
+	
 }
